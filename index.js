0a0062b5d9c1f807e4dc8531946083995bb19f31
Hridoy-shill <ridoyaaa60@gmail.com>
2023-04-30 17:58:00 +0600

    vercel deploy mathod added

--- index.js
+++ index.js
@@ -20,6 +20,27 @@ app.get('/news', (req, res) =>{
     res.send(newsData)
 })
 
+//fine single news by news id 
+app.get('/news/:id', (req, res)=>{
+    const id = req.params.id;
+    const selectedNews = newsData.find(n=> n._id == id)
+    res.send(selectedNews)
+})
+
+// find categories news from newsData by category_id
+app.get('/categories/:id', (req, res)=>{
+    const id = req.params.id;
+    if(id == 0){
+        res.send(newsData)
+    }
+    
+    // find all news
+    else{
+        const categoryNews = newsData.filter(n=>n.category_id == id)
+        res.send(categoryNews)
+    }
+})
+
 app.listen(port, () =>{
     console.log(`Dragon is running on the way:${port}`);
 })
